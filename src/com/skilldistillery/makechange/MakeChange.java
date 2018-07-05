ae662a6f61b92752c0def3506ed55f216a95eb23
Samantha Janik <samajanik@gmail.com>
2018-07-05 14:52:09 -0600

    Added some calculations

--- src/com/skilldistillery/makechange/MakeChange.java
+++ src/com/skilldistillery/makechange/MakeChange.java
@@ -19,6 +19,8 @@ public class MakeChange {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
+		String changeNames[] = { "twenties", "tens", "fives", "ones", 
+				"quarters", "dimes", "nickels", "pennies" }; //names for output
 
 		// Prompt user for price of item
 		System.out.print("Item price: $");
@@ -28,15 +30,12 @@ public class MakeChange {
 		System.out.print("Amount given: $");
 		double payment = sc.nextDouble();
 
-		// Output if amount was too little or exact change
-		checkPayment(price, payment);
-
 		// Call method checkPayment to check amount is <= price & output if either is
 		// true
+		checkPayment(price, payment);
 
 		// If amount tendered is more than item cost, display change and number of bills
-		// and coins
-		// that should be given back.
+		// and coins that should be given back.
 
 		// Call a method to calculate and display change, perhaps within checkPayment
 
@@ -48,6 +47,7 @@ public class MakeChange {
 			// call calculateChange
 			double change = calculateChange(price, payment);
 			System.out.println("Your change is: $" + change);
+			change = changeAmount(change);//might need to change variable on left side?
 		}
 		if (price == payment) {
 			System.out.println("No change needed.");
@@ -62,23 +62,42 @@ public class MakeChange {
 		// check change for bill and coin denominations
 		double change = payment - price;
 
-		String changeNames[] = { "twenties", "tens", "fives", "ones", 
-				"quarters", "dimes", "nickels", "pennies" };
-		
-		double changeAmounts[] = {20.00, 10.00, 5.00, 1.00, 
-				0.25, 0.10, 0.05, 0.01};
 		
 		return change;
 	}
 
-//	public static double changeDollars(double change) {
-//		double changeDollars = 0.0;
-//		// take change and divide by 1 to get cents
-//		// if (change % 1 != 0) {
-//		// }
-//
-//		return changeDollars;
-//	}
+	//need to use changeAmount to calculate how many bills/coins necessary for change
+	//then return the appropriate names in changeNames. Might need to move changeNames to main
+	public static double changeAmount(double change) {
+		//double changeAmount = change;
+		
+		
+		double changeNumbers[] = {20.00, 10.00, 5.00, 1.00, 
+				0.25, 0.10, 0.05, 0.01}; //amounts that go with the names for output
+		
+		//need code to count how many times each of changeAmounts occurs, then output changeNames
+		//for loop?
+		for (int i = 0; i < changeNumbers.length; i++) {
+			if ((int)change % 20 == 0) { //if divisible by 20
+				changeNumbers[0] = (int)change / 20; 
+				//this currently sets first position of array to change/20.
+			}
+			if ((int)change % 10 == 0) {
+				changeNumbers[1] = (int)change / 10;
+			}
+			if ((int)change % 5 == 0) {
+				changeNumbers[2] = (int)change / 5;
+			}
+			changeNumbers[3] = () //need to set 1s to whatever is left
+		}
+		
+		
+		// take change and divide by 1 to get cents
+		// if (change % 1 != 0) {
+		// }
+
+		return change;//possibly returning array
+	}
 //
 //	public static double changeCents(double change) {
 //		// set change to cents value
